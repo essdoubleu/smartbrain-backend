9175f05018e54792ea0d19045a2e4aaa59fbf1e9
Scott Windrem <scottwindrem@gmail.com>
2023-08-28 13:23:17 -0400

    first project commit

---
+++ controllers/profile.js
@@ -0,0 +1,23 @@
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({
+      id,
+    })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0].entries);
+      } else {
+        res.status(400).json("Error getting user.");
+      }
+    });
+
+  // if (!found) {
+  //   res.status(400).json("not found");
+  // }
+};
+
+module.exports = {
+  handleProfileGet: handleProfileGet,
+};
---
+++ controllers/register.js
@@ -0,0 +1,41 @@
+const handleRegister = (req, res, db, bcrypt) => {
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
+  const hash = bcrypt.hashSync(password);
+
+  // bcrypt.compareSync("bacon", hash); // true
+  // bcrypt.compareSync("veggies", hash); // false
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) =>
+    res
+      .status(400)
+      .json("Sorry, we were unable to register.  Please try again later.")
+  );
+};
+
+module.exports = {
+  handleRegister: handleRegister,
+};
